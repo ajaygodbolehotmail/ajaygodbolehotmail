1417dcbf7c3ec500cbe1a3947e04538544ac7d57
ajaygodbolehotmail <ajaygodbole@hotmail.com>
2025-03-23 17:34:36 +0530

    Add files via upload

---
+++ ControlStatements.java
@@ -0,0 +1,20 @@
+package com.ajay.learn;
+
+public class ControlStatements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int age=18;
+			if(age>=18)
+		{
+			System.out.println("Eligibile for vote");
+		}
+		
+		else
+		{
+			System.out.println("Not eligibile");
+		}
+	}
+
+}
